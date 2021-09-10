c32f68db7f82b0e381ed4299b054ab9bf43b4528
NJ <nj.namju@gmail.com>
2021-09-09 20:57:45 -0700

    Update Component_05_SendMessage.cs

--- Introduction to Unity/Assets/2_Component/Component_05_SendMessage.cs
+++ Introduction to Unity/Assets/2_Component/Component_05_SendMessage.cs
@@ -13,11 +13,17 @@ public class Component_05_SendMessage : MonoBehaviour
     {
           // GameObject.FindGameObjectsWithTag helps us to select all geometries which contain the tag in the scope of the Scene. 
           GameObject[] gs = GameObject.FindGameObjectsWithTag("geometry");
+
+          // Debug.Log(gs.Length.ToString());
           
           // loop through the selected geometries and emit the command remotely
           for(int i = 0 ; i < gs.Length; ++i) {
-               gs[i].SendMessage("PrintName", 2.0); 
+               // gs[i].GetComponent<Component_05_SendMessageScripting>().PrintName( i + 3 );
+               gs[i].SendMessage("PrintName", i + 3);
           }
 
+     }
+     void PrintName(int i) {
+          print("from main :" + i.ToString() );
      }
 }
